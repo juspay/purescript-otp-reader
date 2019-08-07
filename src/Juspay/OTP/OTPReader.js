83ec695880dc3770161fc59f9834580aefbdb828
George Varghese M <george.varghese@juspay.in>
2019-08-07 19:12:16 +0530

    What was I thinking..
    Revert the removal of sms poller/receiver because apparently I have terrible prediction skills

--- src/Juspay/OTP/OTPReader.js
+++ src/Juspay/OTP/OTPReader.js
@@ -1,5 +1,52 @@
 var callbackMapper = require('@juspay/mystique-backend').helpers.android.callbackMapper;
 
+exports["getSmsReadPermission'"] = function () {
+  try {
+    var data = DUIGatekeeper.checkReadSMSPermission();
+    var permissions = JSON.parse(data);
+    if(permissions["READ_SMS"] === true && permissions["RECEIVE_SMS"] === true) {
+      return true
+    } else {
+      return false
+    }
+  } catch(e) {
+    //TODO track this
+    return false
+  }
+};
+
+exports["requestSmsReadPermission'"] = function(success) {
+  return function() {
+    var cb = callbackMapper.map(function(params) {
+      var permissions = JSON.parse(params);
+      try {
+        if(permissions["READ_SMS"] === true && permissions["RECEIVE_SMS"] === true) {
+          success(true)();
+        } else {
+          success(false)();
+        }
+      } catch(e) {
+        //TODO track this
+        success(false)();
+      }
+    });
+    DUIGatekeeper.requestSMSPermission(cb);
+  }
+}
+
+exports.startSmsReceiver = function (success) {
+  return function() {
+    var cb = callbackMapper.map(function(data) {
+      success(data)();
+    });
+    DUIGatekeeper.attach("SMS_RECEIVE","{}",cb);
+  }
+};
+
+exports.stopSmsReceiver = function () {
+  DUIGatekeeper.detach(["SMS_RECEIVE"]);
+};
+
 exports.startSmsRetriever = function (success) {
   return function() {
     var cb = callbackMapper.map(function(data) {
@@ -11,6 +58,12 @@ exports.startSmsRetriever = function (success) {
 
 exports.stopSmsRetriever = function () {
   DUIGatekeeper.detach(["SMS_RETRIEVE"]);
+};
+
+exports.readSms = function (time) {
+  return function() {
+    return DUIGatekeeper.fetchFromInbox(time);
+  }
 };
 
 exports.md5Hash = function (s) {
